19e86b1b63028507c8ad7b61f122bd80a63106d4
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-04 23:19:11 -0700

    added public javascript and css files

---
+++ public/js/app.js
@@ -0,0 +1,6 @@
+// DOM Elements
+
+const title = document.getElementById('index-title');
+
+title.addEventListener('mouseover', ()=>title.style.color = 'yellow');
+title.addEventListener('mouseout', ()=>title.style.color = 'orange');
