45ffca24110eecccc1ebcb0e1ea28ba2d4e1bf88
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-07 17:11:25 -0700

    added handlebars template for dynamic pages

--- src/app.js
+++ src/app.js
@@ -4,9 +4,29 @@ const publicDirectoryPath = path.join(__dirname, '../public');
 
 
 const app = express();
+app.set('view engine', 'hbs');
 app.use(express.static(publicDirectoryPath));
 
+app.get('', (req, res)=>{
+  res.render('index', {
+    title: 'Weather', 
+    name: 'Andrew Reynolds'
+  });
+})
 
+
+app.get('/about', (req, res)=>{
+  res.render('about', {
+    title: 'About Me', 
+    name: 'Andrew Reynolds'
+  });
+})
+
+app.get('/help', (req, res)=>{
+  res.render('help', {
+    message: 'Take a look at our FAQ!'
+  })
+});
 
 app.get('/weather', (req,res)=>{
   res.send([
@@ -15,7 +35,7 @@ app.get('/weather', (req,res)=>{
       forecast: 'Sunny with clouds'
     },
     {
-      location: 'Toronoto',
+      location: 'Toronto',
       forecast: 'Overcast and hot'
     }
   ]);
