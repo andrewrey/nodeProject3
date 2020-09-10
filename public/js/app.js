b319ee191b30cc78aa351138f8f782f93f551bd1
Andrew Reynolds <andrew_reynolds@mac.com>
2020-09-10 15:32:28 -0700

    created client side javascript to print weather to screen

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,30 @@
 // DOM element
-const weather = document.querySelector('.weather-info');
+const weatherForm = document.querySelector("form");
+const search = document.getElementById("user-location");
+const weather = document.getElementById("weatherReport");
 
-fetch('http://localhost:3000/weather?address=vancouver')
-  .then(res => res.json())
-  .then(data => {
-    if(data.error)
-    {alert(data.error);
-    } else {
-     weather.innerHTML = data.forecast;
-    }
-  });
+// Event listener
 
+weatherForm.addEventListener("submit", postWeather);
 
+function postWeather(e) {
+  e.preventDefault();
+  let location = search.value;
+  getWeather(location);
+  search.value = "";
+}
 
-
+function getWeather(location) {
+  fetch(`http://localhost:3000/weather?address=${location}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.error) {
+        alert(data.error);
+      } else {
+        weather.innerHTML = `
+      <p>${data.location}</p>
+      <p>${data.forecast}</p>
+      `;
+      }
+    });
+}
